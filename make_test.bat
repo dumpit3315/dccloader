35b9461996bfa1cced452dc1f1d57adbebd5cec3
UserDeploy <thisisnotpremium@gmail.com>
2025-04-15 19:21:01 +0700

    2

--- make_test.bat
+++ make_test.bat
@@ -1,2 +1,2 @@
 @echo off
-clang -DDCC_TESTING -DHAVE_MINILZO=1 -DHAVE_LZ4=1 -D_CRT_SECURE_NO_WARNINGS=1 test/test_rle_compress.c dcc/dn_dcc_proto.c minilzo/minilzo.c lz4/lz4_fs.c plat/wdog.c -o dcc_test_rle.exe
+clang -I . -DDCC_TESTING -DHAVE_MINILZO=1 -DHAVE_LZ4=1 -D_CRT_SECURE_NO_WARNINGS=1 test/test_rle_compress.c dcc/dn_dcc_proto.c minilzo/minilzo.c lz4/lz4_fs.c plat/wdog.c -o dcc_test_rle.exe
