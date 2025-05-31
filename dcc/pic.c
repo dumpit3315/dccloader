553760d906734d85572fa6a34fbad391522ff7db
wrapper <wrapper@system.system>
2025-05-31 22:16:06 +0700

    pic fixes

--- dcc/pic.c
+++ dcc/pic.c
@@ -8,7 +8,7 @@ typedef struct {
 void pic_relocate(uint32_t base, uint32_t reloc_cur, uint32_t reloc_end, uint32_t data_offs) {
     while (reloc_cur < reloc_end) {
         RELOC *rel = (RELOC *)(reloc_cur);
-        if (rel->type == 0x17 && rel->offset >= data_offs) rel->offset += base;
+        if (rel->type == 0x17 && rel->offset >= data_offs) *((uint32_t *)(rel->offset)) += base;
         reloc_cur += sizeof(RELOC);
     }
 }
