d4dd01248c2b5b7ea5c3ded751ae81c2a16bb84b
wrapper <wrapper@system.system>
2025-12-31 13:00:38 +0700

    another signature check

--- tools/dll2has.py
+++ tools/dll2has.py
@@ -14,7 +14,9 @@ if __name__ == "__main__":
     for sect in pe.sections:
         if sect.Name.startswith(b".data"):
             loader_info = bytearray(pe.get_data(sect.VirtualAddress))
-            dcc_signature = loader_info.index(b"&SF1")
+            dcc_signature = loader_info.find(b"&SF1")
+            if dcc_signature == -1: dcc_signature = loader_info.find(b"1.00")
+
             has_init = dcc_signature - 0x15
 
             unlock_offs, unlock_sz, init_offs, init_sz = struct.unpack("<LLLL", loader_info[has_init:has_init+0x10])
